57aabe83ca6f9ee4ba622670194995e8c968cf8c
unknown <lipebrabes@gmail.com>
2023-09-17 21:45:26 -0300

    Add: Using database in login

--- ifish.py
+++ ifish.py
@@ -1,16 +1,39 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "MC426ifish"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         userEmail = request.form["loginEmail"]
+        userPassword = request.form["loginPassword"]
         session["userEmail"] = userEmail
-        return redirect(url_for("user"))
-    else:
-        return render_template("login.html")
+        if(User.query.filter_by(email=userEmail).first() == None):
+            
+            flash('Usuario não existe', category='danger')
+            
+        elif(userPassword == User.query.filter_by(email=userEmail).first().password):
+            return redirect(url_for("user"))
+        else:
+            flash('Senha incorreta', category='danger')
+            
+    
+    return render_template("login.html")
 
 @app.route("/user")
 def user():
